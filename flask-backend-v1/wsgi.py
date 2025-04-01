1d140789d6f5fa8a3bf3a4bddcea7271deeb4507
AtplApplicationsGitHub <66077941+AtplApplicationsGitHub@users.noreply.github.com>
2025-04-01 13:05:58 +0530

    Update wsgi.py

--- flask-backend-v1/wsgi.py
+++ flask-backend-v1/wsgi.py
@@ -4,4 +4,5 @@ from app import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    ssl_context = ('/etc/letsencrypt/live/dev.goval.app/fullchain.pem', '/etc/letsencrypt/live/dev.goval.app/privkey.pem')
+    app.run(host='0.0.0.0', port=5000, debug=True, ssl_context=ssl_context)
