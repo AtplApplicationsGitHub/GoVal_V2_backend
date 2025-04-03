c9d7b5c7600734d2df85cb3211fea4ad1caac0e0
AtplApplicationsGitHub <66077941+AtplApplicationsGitHub@users.noreply.github.com>
2025-04-03 17:44:58 +0530

    Changed CORS Origin to https

--- config.py
+++ config.py
@@ -19,4 +19,4 @@ class Config:
         raise ValueError("JWT_SECRET_KEY is not set in the environment!")
 
     # CORS settings
-    CORS_ORIGINS = os.environ.get('CORS_ORIGINS', 'http://148.113.13.22:*')  # '*' for dev, specify in prod
+    CORS_ORIGINS = os.environ.get('CORS_ORIGINS', 'https://*:*')  # '*' for dev, specify in prod
